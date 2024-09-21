246f6901a618e21ca8a010381b6c0d0b057ebdc4
FridIsar <45432213+FridIsar@users.noreply.github.com>
2024-09-21 17:28:18 +0200

    basic auth passing test + ignore db + multipart requirement

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,4 +1,6 @@
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from . import models
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +11,11 @@ def get_password_hash(password):
 # Verify password
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
-from app.database import engine, Base
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+from . import auth, models, database
 
 app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
 
 @app.get("/up")
 def read_root():
     return {"status": "ok"}
+
+# Dependency
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = auth.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    return {"access_token": "dummy-token", "token_type": "bearer"}
